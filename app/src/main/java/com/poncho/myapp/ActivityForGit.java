d08cd85cf3daf8397aa376a2a60bb2d43d99255a
Yaron Reinharts <yreinharts@seatgeek.com>
2021-03-15 14:47:03 +0200

    Add snack

--- app/src/main/java/com/poncho/myapp/ActivityForGit.java
+++ app/src/main/java/com/poncho/myapp/ActivityForGit.java
@@ -23,6 +23,12 @@ public class ActivityForGit extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own lala", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                Snackbar.make(view, "Replace with your own", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
